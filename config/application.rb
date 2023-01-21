d37499a06d8494fb175dd66f8483fdc352622157
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-21 10:10:46 -0700

    CORS complete

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module TimeSheetsBe
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:3000'
-        resource 'timesheets', headers: :any, methods: [:get, :post]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
 

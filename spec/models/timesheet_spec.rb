3ef1579c9f77937e7b5d5a0bbfe2efcc1a0f1e72
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-20 19:28:22 -0700

    model test for billable hours

--- spec/models/timesheet_spec.rb
+++ spec/models/timesheet_spec.rb
@@ -11,14 +11,20 @@ RSpec.describe Timesheet, type: :model do
     it { should validate_presence_of(:billable_rate) }
   end
 
-  before(:each) do
-    @instance_var = Something.create!(input)
-  end
-
   describe 'class methods' do
-    describe '#search' do
-      it 'returns partial matches' do
-       #method goes here
+    describe '#total_billable_hours' do
+      it 'returns the total billable hours for a given project_code' do
+        Timesheet.destroy_all
+
+        entry_1_company_a_project_a1 = Timesheet.create!(date: "2023-01-20", client: "Company A Name", project: "Project 1 Company A", project_code:"A1", hours: 2.75, billable: true, first_name: "POP", last_name: "TART", billable_rate: 80)
+        entry_2_company_a_project_a1 = Timesheet.create!(date: "2023-01-20", client: "Company A Name", project: "Project 1 Company A", project_code:"A1", hours: 7.75, billable: true, first_name: "POP", last_name: "TART", billable_rate: 80)
+        entry_3_company_a_project_a1 = Timesheet.create!(date: "2023-01-20", client: "Company A Name", project: "Project 1 Company A", project_code:"A1", hours: 7.75, billable: false, first_name: "POP", last_name: "TART", billable_rate: 0)
+        entry_1_company_a_project_a2 = Timesheet.create!(date: "2023-01-20", client: "Company A Name", project: "Project 2 Company A", project_code:"A2", hours: 5.0, billable: false, first_name: "POP", last_name: "TART", billable_rate: 0) 
+        entry_1_company_b_project_b1 = Timesheet.create!(date: "2023-01-20", client: "Company B Name", project: "Project 1 Company B", project_code:"B1", hours: 6, billable: true, first_name: "POP", last_name: "TART", billable_rate: 100)
+        entry_2_company_b_project_b1 = Timesheet.create!(date: "2023-01-20", client: "Company B Name", project: "Project 1 Company B", project_code:"B1", hours: 6.5, billable: true, first_name: "POP", last_name: "TART", billable_rate: 100)
+        entry_3_company_b_project_b1 = Timesheet.create!(date: "2023-01-20", client: "Company B Name", project: "Project 1 Company B", project_code:"B1", hours: 6.5, billable: true, first_name: "POP", last_name: "TART", billable_rate: 100)
+
+        expect(Timesheet.total_billable_hours("A1")).to eq(10.50)
       end
     end
   end

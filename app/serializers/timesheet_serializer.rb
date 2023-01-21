f95c0fb28e0e8a86d51c4dc15ad788f2a1f47254
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-21 09:21:35 -0700

    posting to database with happy path test

--- app/serializers/timesheet_serializer.rb
+++ app/serializers/timesheet_serializer.rb
@@ -9,4 +9,23 @@ class TimesheetSerializer
         end
     }
   end
+
+  def self.new_timesheet(details)
+    {
+    data: 
+      { id: details.project_code,
+        type: "timesheet",
+        attributes: {
+          project_code: details.project_code,
+          hours: details.hours,
+          first_name: details.first_name,
+          last_name: details.last_name,
+          billable: details.billable,
+          billable_rate: details.billable_rate,
+          company_name: details.client,
+          project_name: details.project
+        }
+      }
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :timesheets, only: [:index]
+      resources :timesheets, only: [:index, :create]
     end
   end
 end

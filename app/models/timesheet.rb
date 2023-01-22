9c678314d89649045cde3843d72073e486634ba3
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-22 09:07:41 -0700

    model methods refactored

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,5 +1,13 @@
 class Timesheet < ApplicationRecord
-  validates :date, :client, :project, :project_code, :hours, :first_name, :last_name, :billable_rate, presence: true
+  validates :date,
+            :client,
+            :project,
+            :project_code,
+            :hours,
+            :first_name,
+            :last_name,
+            :billable_rate,
+            presence: true
   validates :billable, inclusion: { in: [ true, false ] }
 
   def self.group_by_project
@@ -7,15 +15,38 @@ class Timesheet < ApplicationRecord
   end
 
   def self.get_project_details(project_code)
-    details = self.where(project_code: project_code)
-    project_details = Hash.new
-    project_details[:id] = details.first.project_code
-    project_details[:project_name] = details.first.project
-    project_details[:client_name] = details.first.client
-    project_details[:total_hours] = details.select(:hours).reduce(0.0) {|sum, entry| sum + entry[:hours]}
-    project_details[:total_billable_amount] = details.where(billable: true).select(:billable_rate, :hours).reduce(0.0) {|sum, entry| sum + entry.billable_rate * entry.hours }
-    project_details[:billable_hours] = details.where(billable: true).select(:hours).reduce(0.0) {|sum, entry| sum + entry.hours }
-    project_details[:billable_percentage] = (project_details[:billable_hours] / project_details[:total_hours] * 100).round(1)
+    timesheets = self.where(project_code: project_code)
+    project_details = build_project_details(timesheets)
     return project_details
   end
+
+  private 
+  def self.build_project_details(timesheets)
+    project_details = {
+      id: timesheets.first.project_code,
+      project_name: timesheets.first.project,
+      client_name: timesheets.first.client,
+      total_hours: total_hours(timesheets),
+      total_billable_amount: total_billable_amount(timesheets),
+      billable_hours: billable_hours(timesheets),
+      billable_percentage: billable_percentage(timesheets)
+    }
+  end
+
+  def self.total_hours(timesheets)
+    timesheets.select(:hours).reduce(0.0) {|sum, entry| sum + entry[:hours]}
+  end
+
+  def self.total_billable_amount(timesheets)
+    timesheets.where(billable: true).select(:billable_rate, :hours).reduce(0.0) {|sum, entry| sum + entry.billable_rate * entry.hours }
+  end
+
+  def self.billable_hours(timesheets)
+    timesheets.where(billable: true).select(:hours).reduce(0.0) {|sum, entry| sum + entry.hours }
+  end
+
+  def self.billable_percentage(timesheets)
+    (billable_hours(timesheets) / total_hours(timesheets) * 100).round(1)
+  end
+
 end

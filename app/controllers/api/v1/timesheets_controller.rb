b1d650922c2ee29540410c174914c34dd96501b6
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-22 08:51:54 -0700

    cleaned up controller to follow rails convention and improve readability and added more tests

--- app/controllers/api/v1/timesheets_controller.rb
+++ app/controllers/api/v1/timesheets_controller.rb
@@ -1,25 +1,28 @@
 class Api::V1::TimesheetsController < ApplicationController
+
   def index
     render json: TimesheetSerializer.serialize_timesheets(Timesheet.all)
   end
 
   def create
-    new_timesheet_details = params["timesheet"]
-    company_info = Timesheet.find_by(project_code: params["timesheet"]["project_code"])
-    timesheet = Timesheet.new(
-      date: Date.today,
-      project_code: new_timesheet_details[:project_code],
-      client: company_info.client,
-      project: company_info.project,
-      hours: new_timesheet_details[:hours],
-      billable: new_timesheet_details[:billable],
-      first_name: new_timesheet_details[:first_name],
-      last_name: new_timesheet_details[:last_name],
-      billable_rate: new_timesheet_details[:billable_rate] )
+    timesheet = Timesheet.new(timesheet_params)
+    timesheet.client = existing_company_info.client
+    timesheet.project = existing_company_info.project
+    timesheet.date = Date.today
     if timesheet.save
-      render json: TimesheetSerializer.new_timesheet(timesheet), status: 201
+      render json: TimesheetSerializer.new_timesheet(timesheet), status: :created
     else
-      render json: { error: "Something went wrong. Check that you have passed in all parameters in the body." }, status: :bad_request
+      render json: { errors: timesheet.errors }, status: :bad_request
     end
   end
+
+  private
+  def existing_company_info
+    Timesheet.find_by(project_code: params["timesheet"]["project_code"])
+  end
+
+  def timesheet_params
+    params.require(:timesheet).permit(:project_code, :hours, :billable, :first_name, :last_name, :billable_rate)
+  end
+
 end
